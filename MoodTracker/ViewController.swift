3269840c17590f7905be4560a08dc72adf73ae90
Lauren Westbrook <lew0105@auburn.edu>
2026-01-29 22:37:32 -0500

    Initial Commit

---
+++ MoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoodTracker
+//
+//  Created by user289249 on 1/29/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
